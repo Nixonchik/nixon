be39058cd8c72ded746d3d4aa2089d0bdc1270d6
Nixonchik <Vladko-91@tut.by>
2016-10-11 19:22:03 +0300

    initial commit

---
+++ src/project/Cat.java
@@ -0,0 +1,13 @@
+package project;
+
+/**
+ * Created by StudentTC on 11.10.2016.
+ */
+public class Cat {
+    int eyes = 2;
+
+    int run() {
+        return 10;
+
+    }
+}
---
+++ src/project/HelloWorld.java
@@ -0,0 +1,16 @@
+package project;
+
+/**
+ * Created by StudentTC on 11.10.2016.
+ */
+public class HelloWorld {
+    public static void main (String[] args ){
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+        Cat cat = new Cat();
+        System.out.println(cat);
+
+
+    }
+}
